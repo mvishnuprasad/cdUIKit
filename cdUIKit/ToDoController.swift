0f428028590e3ecc08740157d1b4153da5dd5ff7
vishnuprasad <vpm735@gmail.com>
2025-06-08 21:17:04 +0530

    init UI

---
+++ cdUIKit/ToDoController.swift
@@ -0,0 +1,31 @@
+//
+//  ToDoController.swift
+//  cdUIKit
+//
+//  Created by vishnuprasad on 08/06/25.
+//
+
+import UIKit
+
+class ToDoController: UITableViewController {
+    let itemArray = ["One", "Two" , "Three"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+}
+
+
+extension ToDoController  {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "toDoItemCell" , for : indexPath)
+        cell.textLabel?.text = itemArray[indexPath.row]
+        return cell
+    }
+}
